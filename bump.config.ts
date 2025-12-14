410fde28d45942655abdbfda69874c6d4204e229
Hiroto Shioi <shioihigg@gmail.com>
2025-12-14 22:34:34 +0900

    ok

--- bump.config.ts
+++ bump.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "bumpp";
 export default defineConfig({
 	// Monorepo: bump root + published packages
 	files: ["package.json", "packages/*/package.json"],
+	recursive: true,
 	all: true,
 	commit: true,
 	tag: true,
